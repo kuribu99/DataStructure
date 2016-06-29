dcd1d2203b6b5ce98dd8d54d8e0e2859767d86cf
KongMy <kuribu99@hotmail.com>
2016-06-29 23:16:07 +0800

    Added answer for Practical Group 2

---
+++ src/main/java/practical/assessment/p2/Q1.java
@@ -0,0 +1,20 @@
+package practical.assessment.p2;
+
+/**
+ *
+ * @author Kong My
+ */
+public class Q1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("calculateInvestment(%d) -> %.2f\n", i, calculateInvestment(1000, 10, i));
+        }
+    }
+
+    public static double calculateInvestment(double investAmount, int interestRate, int year) {
+        if(year <= 0) return investAmount;
+        else return (100 + interestRate) * 0.01 * calculateInvestment(investAmount, interestRate, year - 1);
+    }
+
+}
---
+++ src/main/java/practical/assessment/p2/Q2.java
@@ -0,0 +1,45 @@
+package practical.assessment.p2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kong My
+ */
+public class Q2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter an integer from 0 to 99999: ");
+        String input = scanner.nextLine();
+        try {
+            // Try parse to validate that input is integer
+            int number = Integer.parseInt(input);
+            if (number < 0 || number > 99999) {
+                System.out.println("\nError. Input must be from 0 to 99999");
+            } else {
+                recursivePrint(number);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("\nError: Invalid input.");
+        }
+    }
+
+    public static void recursivePrint(int input) {
+        recursivePrint(input, 10000, 10000);
+    }
+
+    public static void recursivePrint(int input, int currentFactor, int loopFactor) {
+        if (loopFactor == 0) {
+            return;
+        } else if (currentFactor == 0) {
+            System.out.println();
+            recursivePrint(input % loopFactor, loopFactor / 10, loopFactor / 10);
+        } else {
+            System.out.printf("%d   ", (input / currentFactor) % 10);
+            recursivePrint(input, currentFactor / 10, loopFactor);
+        }
+    }
+
+}
