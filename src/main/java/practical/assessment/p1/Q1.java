71d04caaf368c9b668974ee6e4c97c329a0953f7
KongMy <kuribu99@hotmail.com>
2016-06-29 23:15:49 +0800

    Moved answer for Practical Group 1 to package

---
+++ src/main/java/practical/assessment/p1/Q1.java
@@ -0,0 +1,25 @@
+/*
+ */
+package practical.assessment.p1;
+
+/**
+ *
+ * @author Kong My
+ */
+public class Q1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("bunny_ears(%d) -> %d\n", i, bunny_ears(i));
+        }
+    }
+
+    public static int bunny_ears(int n) {
+        if (n <= 0) {
+            return 0;
+        } else {
+            return (n % 2 == 0 ? 3 : 2) + bunny_ears(n - 1);
+        }
+    }
+
+}
