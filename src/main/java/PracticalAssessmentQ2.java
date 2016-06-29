d5f31db4b934eb61641c89d38a684dafda042750
KongMy <kuribu99@hotmail.com>
2016-06-29 20:00:25 +0800

    Added my answer for practical assessment

---
+++ src/main/java/PracticalAssessmentQ2.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ */
+/**
+ *
+ * @author Kong My
+ */
+public class PracticalAssessmentQ2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter an integer: ");
+        String input = scanner.nextLine();
+        try {
+            // Try parse to validate that input is integer
+            int number = Integer.parseInt(input);
+            System.out.printf("decimalCommas(%d) => %s\n", number, decimalCommas(number));
+        } catch (NumberFormatException e) {
+            System.out.println("\nError: Invalid input.");
+        }
+    }
+
+    public static String decimalCommas(int n) {
+        if (Math.abs(n) < 1000) {
+            return String.valueOf(n);
+        } else {
+            return String.format("%s,%03d", decimalCommas(n / 1000), Math.abs(n) % 1000);
+        }
+    }
+
+}
